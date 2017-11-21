667d400dc72ffdefa8a262d601709d953f1d2728
Austen Cameron <austen.cameron@gmail.com>
2017-11-21 13:32:46 -0700

    added highlight.js syntax highlighter, added save button in markdown editor controls

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -9,8 +9,6 @@ window._ = require('lodash');
 
 try {
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
 } catch (e) {}
 
 /**
@@ -39,6 +37,8 @@ if (token) {
 
 // SimpleMDE markdown support
 window.simpleMDE = require('simplemde');
+
+window.hljs = require('highlight.js');
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
--- resources/views/simplemde.blade.php
+++ resources/views/simplemde.blade.php
@@ -1,15 +1,35 @@
 @push('scripts')
 	<script type="text/javascript">
 		new simpleMDE({ 
-
-			@isset ($page)
-			element: document.getElementById('page-content-{{ $page->id }}'),
-			autosave: {
-				enabled: true,
-				uniqueId: 'page-content-' + {{ $page->id }},
+			renderingConfig: {
+				codeSyntaxHighlighting: true
 			},
+
+			toolbar: [
+				'bold', 'italic', 'code', 'quote', '|',
+				'unordered-list', 'ordered-list', '|',
+				'link', 'image', 'table', '|',
+				'preview', 'side-by-side', 'fullscreen', '|',
+				{
+					name: 'save',
+					title: 'Save Page',
+					className: 'fa fa-save',
+					action: function(editor) {
+						$(editor.element).parents('form').submit();
+					},
+				}
+			],
+
+			// Autosaving / element binding based on create / update
+			// It differs on update since the page has an id already
+			@isset ($page)
+				element: document.getElementById('page-content-{{ $page->id }}'),
+				autosave: {
+					enabled: true,
+					uniqueId: 'page-content-' + {{ $page->id }},
+				},
 			@else
-			element: document.getElementById('page-content'),
+				element: document.getElementById('page-content'),
 			@endisset
 		});
 	</script>
