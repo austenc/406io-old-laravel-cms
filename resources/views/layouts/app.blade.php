df49e4a5daa20ca36ab0b7c7f62e79aa08a02eb2
Austen Cameron <austen.cameron@gmail.com>
2017-11-20 19:20:01 -0700

    406.io as default app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', '406.io') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
