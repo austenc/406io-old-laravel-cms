27014c1ba0984479311927efbeb87916e4927540
austenc <austen.cameron@gmail.com>
2018-01-12 00:14:29 -0700

    add new list item on enter press if previous line starts with list item

--- resources/assets/js/mixins/toolbar.js
+++ resources/assets/js/mixins/toolbar.js
@@ -8,9 +8,9 @@ function isMac() {
 
 function cmdOrCtrl(fn) {
     return function (e) {
-      if ((isMac() && e.metaKey) || e.ctrlKey) {
-        return fn.apply(this, arguments)
-      }
+        if ((isMac() && e.metaKey) || e.ctrlKey) {
+            return fn.apply(this, arguments)
+        }
     }
 }
 
@@ -26,9 +26,34 @@ export default {
             this.toggleFullscreen();
         }),
 
-        saveOnEnter: cmdOrCtrl(function() {              
-            this.$refs.editor.form.submit();
-        }),
+        saveOnEnter: function(e) {
+
+            var editor = this.$refs.editor;
+
+            if ((isMac() && e.metaKey) || e.ctrlKey) {
+                e.preventDefault();
+                editor.form.submit();
+            } else {
+                // if this is a list item already, add a new one on the next line
+                var selStart = editor.selectionStart;
+                var lineNumber = editor.value.substr(0, selStart).split('\n').length;
+                var lines = editor.value.split('\n');
+                var currentLine = lines[lineNumber - 1].substring(0, 2);;
+
+                if (currentLine == '- ') {
+                    e.preventDefault();
+                    if (lines[lineNumber - 1].length == 2) {
+                        // remove current line
+                        editor.value = editor.value.substring(0, selStart - 2) 
+                            + editor.value.substring(selStart, editor.value.length);
+                        editor.focus();
+                        editor.selectionEnd = selStart - 2;
+                    } else {
+                        document.execCommand("insertText", false, "\n" + '- ');
+                    }
+                }         
+            }
+        },
 
         bold: cmdOrCtrl(function() {                
             this.insert('**', '**');
