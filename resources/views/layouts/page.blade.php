3f23fcab286eb485f569a51160c7135f89fd97cc
austenc <austen.cameron@gmail.com>
2018-01-12 09:36:47 -0700

    updated logo

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('nav')
-<div class="bg-light p-4 w-full border-t-4 border-brand">
+<div class="bg-grey-lightest p-4 w-full border-t-4 border-brand border-b-none">
     <div class="container mx-auto w-3/4">
-        <a href="{{ url('/') }}">
-            <img src="/img/logo2.png" alt="406.io" class="logo">
+        <a href="{{ url('/') }}" class="logo">
+            <img src="/img/logo.png" alt="406.io"> 406.io
         </a>
     </div>
 </div>
