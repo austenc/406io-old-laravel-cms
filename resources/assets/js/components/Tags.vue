3cf60055409b1a9178baf58c92e2ba4b913b2234
Austen Cameron <austen.cameron@gmail.com>
2018-03-13 15:54:48 -0600

    remove tag feature

--- resources/assets/js/components/Tags.vue
+++ resources/assets/js/components/Tags.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
-        <span class="bg-blue rounded p-1 mr-1" v-for="tag in tags">
+        <span class="text-sm bg-blue rounded p-1 mr-1" v-for="tag in tags">
             {{ tag }}
+            <button @click.prevent="removeTag(tag)" class="text-white">&times;</button>
         </span>
         <input type="text" v-model="tag" placeholder="Enter Tag" class="bg-white p-1 rounded ml-3" @keydown.13.prevent="addTag">
         <input type="hidden" name="tags" v-model="commaTags">
@@ -30,13 +31,18 @@
 
         methods: {
             addTag() {
-                this.tag.split(',').forEach((tag) => { this.tags.push(tag) })
-                this.tag = ''
+                if (this.tag.trim().length > 0) {
+                    this.tag.split(',').forEach((tag) => { this.tags.push(tag) })
+                    this.tag = ''                    
+                }
+            },
+            removeTag(tag) {
+                this.tags = _.without(this.tags, tag)                
             }
         },
 
         mounted() {
-            this.tags = this.for;
+            this.tags = this.for
         }
     }    
 </script>
